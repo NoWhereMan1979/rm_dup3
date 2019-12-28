fb3a40eaf3c247ea884f37507b261cbee12b92ec
h.Abbasi <ha58pnu@gmail.com>
2019-12-28 17:15:27 +0330

    execution flags added. each step went into a function outside of main(). OS and FS errors like existing non empty directory or execution directory existance error, handled

--- rm_dup3.go
+++ rm_dup3.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -8,32 +9,57 @@ import (
 	"regexp"
 )
 
-var wd = "/home/happy/hdd460/hae/solopianoradio/Whisperings- Solo Piano Radio"
-var match = regexp.MustCompile(`\s\([^\d]*(\d+)[^\d]*\)`)
+var path = flag.String("p", wd, "execution path")
+var regex = flag.String("r", regexStr, "duplicated file names addition pattern regexp. default means \" (digits)\"")
+var mvDest = flag.String("m", movingDestination, "moving destination directory")
+
+var wd = "./"
+var movingDestination = "mvd"
+var regexStr = `\s\([^\d]*(\d+)[^\d]*\)`
+var match = regexp.MustCompile(regexStr)
+var spash = `File name dupplication remover
+Chooses most complete files from streamripper dupplicate ripped files.
+Then renames to original file name and moves them to a directory
+Use with -h for usage help`
 
 func main() {
-	err := os.Chdir(wd)
-	if err != nil {
-		panic(err)
-	}
+	fmt.Println(spash)
+	flag.Parse()
+	wd = *path
+	regexStr = *regex
+	match = regexp.MustCompile(regexStr)
+	chDir(wd)
+	makeDir(movingDestination)
+	groupMap := readNGroup()
+	lastList := chooseSuits(groupMap)
+	renameNMove(lastList)
+}
+
+func readNGroup() map[string][]os.FileInfo {
 	files, err := ioutil.ReadDir(wd)
 	if err != nil {
 		log.Fatal(err)
 	}
 	groupMap := make(map[string][]os.FileInfo)
 	for _, f := range files {
-		var nameF = f.Name()
-		nameF = trim(nameF)
-		if gr, ok := groupMap[nameF]; ok {
-			gr = append(gr, f)
-			groupMap[nameF] = gr
-		} else {
-			var g []os.FileInfo
-			g = append(g, f)
-			groupMap[nameF] = g
+		if !f.IsDir() {
+			var nameF = f.Name()
+			nameF = trim(nameF)
+			if gr, ok := groupMap[nameF]; ok {
+				gr = append(gr, f)
+				groupMap[nameF] = gr
+			} else {
+				var g []os.FileInfo
+				g = append(g, f)
+				groupMap[nameF] = g
+			}
 		}
 	}
 	fmt.Println(len(groupMap))
+	return groupMap
+}
+
+func chooseSuits(groupMap map[string][]os.FileInfo) []os.FileInfo {
 	var lastList []os.FileInfo
 	for _, g := range groupMap {
 		var selected os.FileInfo
@@ -46,19 +72,54 @@ func main() {
 		}
 		lastList = append(lastList, selected)
 	}
-	var dirName = "./pldnufhsd"
-	e := os.Mkdir(dirName, 0777)
-	if e != nil {
-		os.Exit(14)
-	}
+	return lastList
+}
+
+func renameNMove(lastList []os.FileInfo) {
+	var count = 0
 	for _, x := range lastList {
-		rer := os.Rename(x.Name(), dirName+"/"+trim(x.Name()))
+		rer := os.Rename(x.Name(), movingDestination+"/"+trim(x.Name()))
 		if rer != nil {
-			fmt.Println("error moving file ", x.Name(), "to", dirName+"/"+trim(x.Name()))
+			fmt.Println("error moving file ", x.Name(), "to", movingDestination+"/"+trim(x.Name()))
+			continue
+		}
+		count++
+	}
+	fmt.Println("moved files", count)
+}
+
+func makeDir(dir string) {
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		e := os.Mkdir(dir, 0777)
+		if e != nil {
+			fmt.Println(e.Error())
+			os.Exit(1)
+		}
+	} else {
+		files, err := ioutil.ReadDir(dir)
+		if err != nil {
+			fmt.Println(err.Error())
+			os.Exit(2)
+		}
+		if len(files) != 0 {
+			fmt.Println("directory", dir, "exists and is not empty")
+			os.Exit(3)
 		}
 	}
-	fmt.Println("moved files", len(lastList))
 }
+
+func chDir(dir string) {
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	err := os.Chdir(dir)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(2)
+	}
+}
+
 func trim(n string) string {
 	var loc = match.FindStringIndex(n)
 	if loc != nil {
